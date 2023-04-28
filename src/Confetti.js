3c4785d0a83fd977dc76f4eb9ae19775117767de
Tarun <tarunbanga4565@gmail.com>
2023-04-28 22:23:04 +0530

    first commit

---
+++ src/Confetti.js
@@ -0,0 +1,16 @@
+
+import Confetti  from "react-confetti";
+ 
+export default ()=>{
+    return  <div>
+         <Confetti 
+         
+        //    width = {400}
+           
+        //    heigt = {400}
+        gravity={0.5}
+        particleCount = {100}
+           />
+    </div>
+}
+  
